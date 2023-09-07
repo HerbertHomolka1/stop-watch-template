981ae4250530d7570e75515736b1348e599706c3
HerbertHomolka1 <herberthomolka0@gmail.com>
2023-09-07 22:33:26 +0200

    demo

--- src/dom.js
+++ src/dom.js
@@ -1,2 +1,11 @@
-const dom = {};
-export default dom;
+const dom = {
+    startTimerBlock: document.getElementById("startTimerBlock"),
+    stopTimerBlock: document.getElementById("stopTimerBlock"),
+    resetTimerBlock: document.getElementById("resetTimerBlock"),
+    minuteBlock: document.getElementById("minuteBlock"),
+    secondBlock: document.getElementById("secondBlock"),
+    millisecondBlock: document.getElementById("millisecondBlock")
+  };
+  
+  export default dom;
+  
---
+++ src/handlers/handlers.js
@@ -0,0 +1,38 @@
+import dom from '../dom.js';
+import data from '../data.js';
+import updateValues from '../utils/updateValues.js';
+
+let { minutes, seconds, milliseconds } = data; 
+
+
+let timerInterval; 
+
+function startTimer() {
+    timerInterval = setInterval(() => {
+        milliseconds += 10;
+        if (milliseconds === 1000) {
+            milliseconds = 0;
+            seconds += 1;
+        }
+        if (seconds === 60) {
+            seconds = 0;
+            minutes += 1;
+        }
+        updateValues(minutes, seconds,milliseconds) ;
+    }, 10);
+}
+
+function stopTimer() {
+    clearInterval(timerInterval); 
+}
+
+function resetTimer() {
+    seconds = 0;
+    milliseconds = 0;
+    minutes = 0;
+    updateValues(minutes, seconds,milliseconds) ;
+}
+
+
+
+export { startTimer, stopTimer, resetTimer };
