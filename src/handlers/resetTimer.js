c4999e44a279ff703505092b510b50a6bb5e2208
HerbertHomolka1 <herberthomolka0@gmail.com>
2023-09-09 12:40:26 +0200

    better number showing

--- src/handlers/resetTimer.js
+++ src/handlers/resetTimer.js
@@ -1,14 +1,12 @@
-import data from '../data.js';
-import updateValues from '../utils/updateValues.js';
+import data from "../data.js";
+import updateValues from "../utils/updateValues.js";
 
 function resetTimer() {
- 
-    data.seconds = 0;
-    data.milliseconds = 0;
-    data.minutes = 0;
+  data.seconds = 0;
+  data.milliseconds = 0;
+  data.minutes = 0;
 
-    updateValues(data.minutes, data.seconds, data.milliseconds);
-   
+  updateValues(data.minutes, data.seconds, data.milliseconds);
 }
 
 export default resetTimer;
--- src/handlers/stopTimer.js
+++ src/handlers/stopTimer.js
@@ -1,10 +1,10 @@
 import data from "../data.js";
 
 function stopTimer() {
-    if (data.timerInterval) {
-        clearInterval(data.timerInterval);
-        data.timerInterval = null ;
-    }
+  if (data.timerInterval) {
+    clearInterval(data.timerInterval);
+    data.timerInterval = null;
+  }
 }
 
 export default stopTimer;
